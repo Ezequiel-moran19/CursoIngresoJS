0c8490396e8b240df69e6c38d859e7e044729e4f
unknown <morancito.91@gmail.com>
2021-07-06 14:34:04 -0300

    realice ejercicios 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,22 +4,75 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	alert("ok");	
+{	var num1;
+	var num2;
+    var Rdo;
+
+    num1= parseInt(document.getElementById("txtIdNumeroUno").value);
+    // convertimos "txtIdnumeroUno"a numerica
+
+    num2= parseInt(document.getElementById("txtIdNumeroDos").value);
+    // convertimos "txtIdnumeroDos"a numerica
+
+    Rdo= num1 + num2;
+
+    alert("la suma es: " + Rdo);
+
+	alert("");	
 }
 
 function restar()
 {
-	
+	var num1;
+	var num2;
+    var Rdo;
+
+    num1= parseInt(document.getElementById("txtIdNumeroUno").value);
+    // convertimos "txtIdnumeroUno"a numerica
+
+    num2= parseInt(document.getElementById("txtIdNumeroDos").value);
+    // convertimos "txtIdnumeroDos"a numerica
+
+    Rdo= num1 - num2;
+
+    alert("la resta es: " + Rdo);
+
 }
 
 function multiplicar()
 { 
-	
+	var num1;
+	var num2;
+    var Rdo;
+
+    num1= parseInt(document.getElementById("txtIdNumeroUno").value);
+    // convertimos "txtIdnumeroUno"a numerica
+
+    num2= parseInt(document.getElementById("txtIdNumeroDos").value);
+    // convertimos "txtIdnumeroDos"a numerica
+
+    Rdo= num1 * num2;
+
+    alert("la multiplicacion es: " + Rdo);
+
 }
 
 function dividir()
 {
+	var num1;
+	var num2;
+    var Rdo;
+
+    num1= parseInt(document.getElementById("txtIdNumeroUno").value);
+    // convertimos "txtIdnumeroUno"a numerica
+
+    num2= parseInt(document.getElementById("txtIdNumeroDos").value);
+    // convertimos "txtIdnumeroDos"a numerica
+
+    Rdo= num1 / num2;
+
+    alert("la division es: " + Rdo);
+
 	
 }
 
