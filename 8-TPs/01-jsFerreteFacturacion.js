c6891b0c215fa957898e8319863d51df4410364a
unknown <morancito.91@gmail.com>
2021-07-07 13:37:54 -0300

    realice TP 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,75 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+   var producto1 ;
+   var producto2;
+   var producto3;
+   var suma;
+   // declaro valores
+
+	 producto1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    producto2=parseInt(document.getElementById("txtIdPrecioDos").value);
+    producto3=parseInt(document.getElementById("txtIdPrecioTres").value);
+	//a traves de document traemos en input para luego parsearlo
+	 
+   suma= producto1 + producto2 + producto3;
+   //sumanos los tres productos
+
+   alert("la suma de los tres productos es: " + suma );
+
+
 }
+
 function Promedio () 
 {
-	
+	var producto1 ;
+   var producto2;
+   var producto3;
+   var suma;
+   var promedio;
+   // declaro valores
+
+    producto1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    producto2=parseInt(document.getElementById("txtIdPrecioDos").value);
+    producto3=parseInt(document.getElementById("txtIdPrecioTres").value);
+   //a traves de document traemos en input para luego parsearlo
+    
+
+   suma= producto1 + producto2 + producto3;
+   promedio= suma /3;
+   //promedio de los tres productos
+   
+   alert("El promedio de los tres productos es: " + promedio );
+
 }
 function PrecioFinal () 
 {
-	
+	var producto1 ;
+   var producto2;
+   var producto3;
+   var preciofinal;
+   // declaro valores
+
+    producto1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    producto2=parseInt(document.getElementById("txtIdPrecioDos").value);
+    producto3=parseInt(document.getElementById("txtIdPrecioTres").value);
+   //a traves de document traemos en input para luego parsearlo
+    
+
+    
+    suma= producto1 + producto2 + producto3 ;
+    preciofinal= (suma * 0.21)  + suma;
+   //promedio de los tres productos
+   
+   alert("El prescio final es: " + preciofinal );
+
+
 }
+
+/*
+     producto1= 100;
+     producto2= 150;
+     producto3= 200;
+   esto no se hace es hardcodear ** estas asignando valores fijos a tus variables
+
+    */
